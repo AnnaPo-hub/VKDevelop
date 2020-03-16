662ebba2855e55df8b7d9353d5d3fc1002d9a014
Anna Popova <anna.popova201909@gmail.com>
2020-03-16 13:46:56 +0300

    Initial commit #v1

---
+++ src/test/java/PostData.java
@@ -0,0 +1,13 @@
+public class PostData {
+
+    private int postID;
+    private String postAuthor;
+    private String postTime;
+    private boolean advertise;
+    private String imageURL;
+    private int viewQuantity;
+    private int likeQuantity;
+    private int commentQuantity;
+
+
+}
